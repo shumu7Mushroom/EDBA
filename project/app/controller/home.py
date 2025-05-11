12e6015db38b3ab9412592d75dfc96c0cc1b07d0
shumu7Mushroom <18825321509@163.com>
2025-05-11 13:32:10 +0800

    now user can see and download rules file in index page

--- project/app/controller/home.py
+++ project/app/controller/home.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, redirect, url_for, render_template
+from app.models.rule import Rule
 
 mainBP = Blueprint('main', __name__)
 
 @mainBP.route('/')
 def index():
     return render_template('index.html', title="欢迎使用 E-DBA 系统")
+
+@mainBP.route('/rules/view')
+def view_rules():
+    rules = Rule.query.all()
+    return render_template('view_rules.html', rules=rules)
